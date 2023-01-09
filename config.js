536ff6aa8c29a6e37203a6bd907b1e250c7b22f2
MhNirra <72897503+MhNirra@users.noreply.github.com>
2023-01-08 23:20:10 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var full_config = {
+    countdown: {
+        date: '2022-04-14 00:00:00',
+        foreground: '#fff',
+        background: 'rgba(0, 0, 0, .15)',
+    },
+    twitter: 'mhnirra_mhnirra'
+};
